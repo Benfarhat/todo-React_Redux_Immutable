8bc847a23f15c58dda64061d5b5f75352555f428
Benfarhat <benfarhat.elyes@gmail.com>
2018-02-18 21:40:14 +0100

    initial commit

---
+++ src/actions.js
@@ -0,0 +1,19 @@
+const uid = () => require('crypto').randomBytes(10).toString('hex')
+
+export function addTodo(text) {
+    return {
+        type: 'ADD_TODO',
+        payload: {
+            id: uid(),
+            isDone: false,
+            text: text
+        }
+    }
+}
+
+export function toggleTodo(id) {
+    return {
+        type: 'TOGGLE_TODO',
+        payload: id
+    }
+}
